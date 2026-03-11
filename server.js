6a23c581cfebe1165085838a4cee515601e552eb
Whiksy <phobo0hanh@gmail.com>
2026-03-11 08:20:20 +0700

    Thêm Server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Flower Shop đang chạy tại http://localhost:${PORT}`);
+});

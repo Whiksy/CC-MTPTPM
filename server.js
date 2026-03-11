ad8a48d0f8ace0c1dce0809a17119235354e866f
VuDucThien <thienvuduc027@gmail.com>
2026-03-11 08:52:29 +0700

    Them api flower

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ const path = require('path');
 const PORT = 3000;
 
 app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/flowers', (req, res) => {
+    const flowers = [
+        { id: 1, name: 'Hoa Hồng', price: 15 },
+        { id: 2, name: 'Hoa Lan', price: 30 },
+        { id: 3, name: 'Hoa Cúc', price: 10 },
+        { id: 4, name: 'Hoa Ly', price: 25 },
+        { id: 5, name: 'Hoa Hướng Dương', price: 20 }
+    ];
+    res.json(flowers);
 });
 
 app.listen(PORT, () => {
